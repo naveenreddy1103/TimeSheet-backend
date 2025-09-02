3f4809791f9b8f8f9e5c7b3341c81ec62e119df0
knr12 <knr123456@example.com>
2025-09-02 16:22:19 +0530

    small string modification

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const dotenv=require('dotenv').config
 
 const dbConnect=async()=>{
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL||"mongodb+srv://Nodejs:Nodejs@cluster0.r72zfn1.mongodb.net/TimeStamp")
 }
 
 module.exports=dbConnect
